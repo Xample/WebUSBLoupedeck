8acbde45d1cfd22174979d90dede9959733d8abf
Flavien Volken <>
2023-01-28 16:27:38 +0100

    Test RX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,52 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {delayWhen, Observable, repeat, shareReplay, switchMap, tap} from "rxjs";
+import {fromPromise} from "rxjs/internal/observable/innerFrom";
+import {doOnUnsubscribe} from "../observable.util";
+
+
+class LoupedeckDevice {
+  private static loupedeckVendorId = 0x2ec2;
+  private device$: Observable<USBDevice>;
+
+  constructor() {
+    this.device$ = LoupedeckDevice.buildDeviceInterface$();
+  }
+
+  // todo: add ability to forget the device on unsubscribe()
+  private static getDevice$(): Observable<USBDevice> {
+    return fromPromise(navigator.usb.requestDevice({filters: [{vendorId: this.loupedeckVendorId}]}));
+  }
+
+  /**
+   * Open the device on subscribe, automatically close it on unsubscribe
+   */
+  private static captureDevice$(): Observable<USBDevice> {
+    let capturedDevice: USBDevice;
+    return this.getDevice$().pipe(
+      tap((device) => capturedDevice = device),
+      delayWhen((device) => fromPromise(device.open())),
+      //doOnUnsubscribe(() => {capturedDevice.close()})
+    )
+  }
+
+  private static buildDeviceInterface$(): Observable<USBDevice> {
+    return this.captureDevice$().pipe(
+      delayWhen((device) => fromPromise(device.selectConfiguration(1))),
+      delayWhen((device) => fromPromise(device.claimInterface(1))),
+      shareReplay({refCount: true, bufferSize: 1}),
+    );
+  }
+
+  read$() {
+
+    return this.device$.pipe(
+      switchMap((device) => {
+        return fromPromise(device.transferIn(0x01, 512))
+      }),
+      repeat())
+  }
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +55,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loupedeck';
+
+  async onStartClicked() {
+
+    const loupedeck = new LoupedeckDevice();
+    loupedeck.read$().subscribe((value)=>console.log(value),()=>console.log("error"))
+
+  }
 }
+
---
+++ src/observable.util.ts
@@ -0,0 +1,56 @@
+import { defer, Observable } from 'rxjs';
+import { finalize, first, share, take } from 'rxjs/operators';
+
+/**
+ * https://stackoverflow.com/a/48983205/532695
+ *
+ * Example
+ import {from} from 'rxjs/observable/from';
+ 
+ from([1, 2, 3])
+ .pipe(doOnSubscribe(() => console.log('subscribed to stream')))
+ .subscribe(x => console.log(x), null, () => console.log('completed'));
+ */
+
+export function doOnSubscribe<T>(onSubscribe: () => void): (source: Observable<T>) => Observable<T> {
+  return function inner(source: Observable<T>): Observable<T> {
+    return defer(() => {
+      onSubscribe();
+      return source;
+    });
+  };
+}
+
+/***
+ * Example
+ import {from} from 'rxjs/observable/from';
+
+ from([1, 2, 3])
+ .take(1)
+ .pipe(doOnUnsubscribe(() => console.log('unsubscribed from stream')))
+ .subscribe(x => console.log(x), null, () => console.log('completed'));
+ */
+
+export function doOnUnsubscribe<T>(onUnsubscribe: () => void): (source: Observable<T>) => Observable<T> {
+  return function inner(source: Observable<T>): Observable<T> {
+    // Possible shorter alternative: return source.pipe(finalize(onUnsubscribe), share()); see https://github.com/ReactiveX/rxjs/issues/3786#issuecomment-394523704
+    return new Observable((subscriber) => {
+      const subscription = source.subscribe(
+        (value) => {
+          subscriber.next(value);
+        },
+        (value) => {
+          subscriber.error(value);
+        },
+        () => {
+          subscriber.complete();
+        }
+      );
+
+      return () => {
+        subscription.unsubscribe();
+        onUnsubscribe();
+      };
+    });
+  };
+}
